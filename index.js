846c4fb19e7bc35f3127617caf07e081e159b75f
Edith Gatwiri <edithgatwiri70@gmail.com>
2024-12-19 23:20:05 +0300

    dodger

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+
+  // define dodger element
+const dodger = document.getElementById("dodger");
+
+// Move the dodger left
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+// Move the dodger right
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) { 
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+// Add event listeners 
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
